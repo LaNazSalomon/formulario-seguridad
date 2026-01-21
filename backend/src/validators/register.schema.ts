20c0826ae672f1d900d80d6fd7de52689e4c0eb8
lans <nazarsalomon123@gmail.com>
2026-01-20 21:03:13 -0600

    Se agregaron indicaciones de que se hace mal si algo sale mal

--- backend/src/validators/register.schema.ts
+++ backend/src/validators/register.schema.ts
@@ -2,27 +2,27 @@ import { z } from 'zod';
 
 export const registerSchema = z.object({
   nombre: z
-    .string()
+    .string('El nombre tiene que ser texto')
     .min(2, 'Nombre muy corto')
     .max(100, 'Nombre muy largo')
     .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, 'Nombre inválido'),
 
   apellido: z
-    .string()
-    .min(2)
-    .max(100)
+    .string('El apellido tiene que ser texto')
+    .min(2, 'minimo debe contener dos letras el apellido')
+    .max(100, 'El maximo de caracteres permitidos para el apellido es cien')
     .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, 'Apellido inválido'),
 
   usuario: z
-    .string()
-    .min(4)
-    .max(30)
+    .string('El usuario tiene que ser una cadena de texto')
+    .min(4, 'Nombre de usuario por lo menos debe tener 4 caracteres')
+    .max(30, 'El maximo de caracteres es de 30 en el nombre de usuario')
     .regex(/^[a-zA-Z0-9_]+$/, 'Usuario inválido'),
 
   email: z.string().email('Email inválido'),
 
   password: z
-    .string()
+    .string('La contrasena no puede estar vacia')
     .min(8, 'Contraseña muy corta')
     .max(100)
     .regex(/[A-Z]/, 'Debe tener una mayúscula')
@@ -34,7 +34,7 @@ export const registerSchema = z.object({
 
 edad: z
   .coerce
-  .number()
+  .number('La edad debe ser un numero')
   .int('La edad debe ser un número entero')
   .min(13, 'Edad mínima 13')
   .max(120, 'Edad máxima 120')
@@ -42,7 +42,7 @@ edad: z
 
 
   telefono: z
-    .string()
+    .string('El telefono no puede estar vacio')
     .regex(/^[0-9]{10}$/, 'Teléfono inválido')
     .optional(),
 }).refine(
